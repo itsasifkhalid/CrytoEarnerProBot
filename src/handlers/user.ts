ecc50afe0b2738c58e373593892e474c5966a356
Vilen Kuprienko <pride2808@gmail.com>
2019-06-21 22:51:17 +0300

    contact handler was added

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,10 +1,16 @@
 import * as api from 'telegraf'
+import ContactsMessage from '../controllers/contacts'
 import InvestMessage from '../controllers/userInvest'
 
 export default class User {
     public static init(bot: api.Telegraf<api.ContextMessageUpdate>) {
-        bot.hears('Инвестировать', async (ctx: any) => {
+        // TODO: Поменять эмоджи в параметрах
+        bot.hears('💼 Инвестировать', async (ctx: any) => {
             await InvestMessage.send(ctx)
+        })
+
+        bot.hears('💼 Контакты', async (ctx: any) => {
+            await ContactsMessage.send(ctx)
         })
     }
 }
