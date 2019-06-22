694d0eb4dd65675d167944f558bdf905cf385cdb
Vilen Kuprienko <pride2808@gmail.com>
2019-06-22 22:28:47 +0300

    Test data from our api is rendering now

--- admin/src/components/App.tsx
+++ admin/src/components/App.tsx
@@ -1,12 +1,55 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
 
-export default class App extends Component {
+export interface AppState {
+    users:
+        [
+            {
+                id: number
+                name: string
+                email: string
+            }
+        ]
+}
+
+export default class App extends Component<{}, AppState> {
+    constructor(props: any) {
+        super(props)
+
+        this.state = {
+            users: [{
+                id: 0,
+                name: 'loading',
+                email: 'loading'
+            }]
+        }
+    }
+
+
     render() {
+        const users = this.state.users.map((user, index) => {
+            return (
+                <div key={user.id} className='card'>
+                    <h2>{user.name}</h2>
+                    <h3>{user.email}</h3>
+                    <hr/>
+                </div>
+            )
+        })
+
         return (
             <div>
-                <h1>Hello from admin!</h1>
+                {users}
             </div>
         )
+    }
+
+    async componentDidMount() {
+        let response = await fetch('http://127.0.0.1:8888')
+        let data = await response.json()
+
+        this.setState({
+            users: data
+        })
     }
 }
