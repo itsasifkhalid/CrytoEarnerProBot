7b633b018b64ec8001fc3ff16533a6d321b434c5
Vilen Kuprienko <pride2808@gmail.com>
2019-06-22 19:44:15 +0300

    Admin panel was added

---
+++ admin/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../styles/App.css'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from admin!</h1>
+            </div>
+        )
+    }
+}
