28a1a2f581cb4a487a67e5c9bcea8a1e762423e9
Vilen Kuprienko <pride2808@gmail.com>
2019-06-23 14:32:41 +0300

    Sider was added

--- admin/src/components/App.tsx
+++ admin/src/components/App.tsx
@@ -1,55 +1,13 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
+import Sider from './Sider'
 
-export interface AppState {
-    users:
-        [
-            {
-                id: number
-                name: string
-                email: string
-            }
-        ]
-}
-
-export default class App extends Component<{}, AppState> {
-    constructor(props: any) {
-        super(props)
-
-        this.state = {
-            users: [{
-                id: 0,
-                name: 'loading',
-                email: 'loading'
-            }]
-        }
-    }
-
-
+export default class App extends Component {
     render() {
-        const users = this.state.users.map((user) => {
-            return (
-                <div key={user.id} className='card'>
-                    <h2>{user.name}</h2>
-                    <h3>{user.email}</h3>
-                    <hr/>
-                </div>
-            )
-        })
-
         return (
             <div>
-                {users}
+                <Sider />
             </div>
         )
-    }
-
-    async componentDidMount() {
-        let response = await fetch('http://127.0.0.1:8888')
-        let data = await response.json()
-
-        this.setState({
-            users: data
-        })
     }
 }
