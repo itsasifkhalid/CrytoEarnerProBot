2557798ccef83f9a3daebe31351f5da363a80b8b
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-06-26 14:05:49 +0300

    Little changes.

--- bot/src/api/handlers/handlers.ts
+++ bot/src/api/handlers/handlers.ts
@@ -5,65 +5,65 @@ import { addInvestment } from '../../helpers/invest'
 export const stats = {
 	async all(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
 		try {
 			const all = await db.stats.getAll();
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.header('Content-Type', 'application/json');
 			res.end(all);
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
 	async balance(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
 		try {
 			const balance = await db.stats.getBalance();
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.header('Content-Type', 'application/json');
 			res.end(balance);
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
 	async investorsAmount(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
 		try {
 			const amount = await db.stats.getInvestorsAmount();
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.header('Content-Type', 'application/json');
 			res.end(amount);
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
 	async investorsToday(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
 		try {
 			const amountToday = await db.stats.getInvestorsAmountToday();
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.header('Content-Type', 'application/json');
 			res.end(amountToday);
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	}
@@ -72,46 +72,46 @@ export const stats = {
 export const investors = {
 	async all(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
 		try {
 			const investors = await db.investors.getInvestors();
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.header('Content-Type', 'application/json');
 			res.end(investors);
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
 	async investor(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
 		const { username } = req.query;
 		try {
 			const investor = await db.investors.getInvestor(username);			
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.header('Content-Type', 'application/json');
 			res.end(investor);
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
 	async addInvestment(req: express.Request, res: express.Response): Promise<void> {
 		const { id, chatId } = req.query;
 		addInvestment(id, chatId);
-		res.header('StatusCode', 200)
+		res.header('StatusCode', '200')
 		res.end(JSON.stringify({}));
 	},
 	async setInvestorStatus(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
@@ -119,16 +119,16 @@ export const investors = {
 		status = status.toUpperCase();
 		try {
 			await db.investors.setInvestorStatus(username, status);
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.end(JSON.stringify({}));
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
 	async setInvestmentStatus(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
@@ -136,26 +136,26 @@ export const investors = {
 		status = status.toUpperCase();
 		try {
 			await db.investors.setInvestmentStatus(username, id, status);
-			res.header('StatusCode', 200);
+			res.header('StatusCode', '200');
 			res.end(JSON.stringify({}));
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
-	async setInvestorNote(req: express.Request, res: express.Response): Promise<void> {
+	async setInvestorStatus(req: express.Request, res: express.Response): Promise<void> {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
 		let { username, note } = req.body;
 		try {
-			await db.investors.setInvestorNote(username, note);
-			res.header('StatusCode', 200);
+			await db.investors.setInvestorNote(username, status);
+			res.header('StatusCode', '200');
 			res.end(JSON.stringify({}));
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	}
@@ -164,11 +164,11 @@ export const investors = {
 export const auth = {
 	check(req: express.Request, res: express.Response): void {
 		if (!req.session.authorized) {
-			res.header('StatusCode', 401);
+			res.header('StatusCode', '401');
 			res.end(JSON.stringify({}));
 			return;
 		}
-		res.header('StatusCode', 200);
+		res.header('StatusCode', '200');
 		res.end(JSON.stringify({}));
 	},
 	async signIn(req: express.Request, res: express.Response): Promise<void> {
@@ -180,21 +180,21 @@ export const auth = {
 				req.session.authorized = true;
 				req.session.username = username;
 
-				res.header('StatusCode', 200);
+				res.header('StatusCode', '200');
 				res.end(JSON.stringify({}));
 			} else {
-				res.header('StatusCode', 401);
+				res.header('StatusCode', '401');
 				res.end(JSON.stringify({}));
 			}
 		} catch (err) {
-			res.header('StatusCode', 500);
+			res.header('StatusCode', '500');
 			res.end('Error: Internal server error');
 		}
 	},
 	async signOut(req: express.Request, res: express.Response): Promise<void> {
 		delete req.session.authorized;
 		delete req.session.username;
-		res.header('StatusCode', 200);
+		res.header('StatusCode', '200');
 		res.end(JSON.stringify({}));
 	}
 }
